4d73cc481b5fa83a8705fbec327935bb1f8246f0
Pezhman Nasirifard <epezhman@gmail.com>
2017-08-14 17:54:29 +0200

    first commit

---
+++ app/config.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const appPackage = require('./package.json');
+const APP_SHORT_NAME = appPackage.name;
+const APP_NAME = appPackage.productName;
+const APP_VERSION = appPackage.version;
+const IS_DEVELOPMENT = process.env.NODE_ENV === 'development';
+const IS_WINDOWS = process.platform === 'win32';
+const IS_LINUX = process.platform === 'linux';
+
+module.exports = {
+	APP_COPYRIGHT: 'Copyright © 2017 ' + APP_NAME,
+	APP_SHORT_NAME: APP_SHORT_NAME,
+	APP_NAME: APP_NAME,
+	APP_VERSION: APP_VERSION,
+	IS_WINDOWS: IS_WINDOWS,
+	IS_LINUX: IS_LINUX,
+	IS_DEVELOPMENT: IS_DEVELOPMENT,
+};
---
+++ app/main/index.js
@@ -0,0 +1,81 @@
+'use strict';
+const electron = require('electron');
+
+
+const app = electron.app;
+
+// Adds debug features like hotkeys for triggering dev tools and reload
+require('electron-debug')();
+
+// Prevent window being garbage collected
+let publishWindow;
+let subscribeWindow;
+
+function onClosedPublish() {
+	publishWindow = null;
+}
+
+function createPublishWindow() {
+	const win = new electron.BrowserWindow({
+		backgroundColor: '#ECECEC',
+		center: true,
+		title: "i13 Publisher",
+		icon: "../ assets/img/p.png",
+		useContentSize: true,
+		width: 800,
+		height: 400,
+		x: 0,
+		y: 0
+	});
+
+	win.loadURL(`file://${__dirname}/../renderer/publish.html`);
+	win.on('closed', onClosedPublish);
+
+	return win;
+}
+
+function onClosedSubscribe() {
+	subscribeWindow = null;
+}
+
+function createSubscribeWindow() {
+	const win = new electron.BrowserWindow({
+		backgroundColor: '#ECECEC',
+		center: true,
+		title: "i13 Subscriber",
+		icon: "../ assets/img/s.png",
+		useContentSize: true,
+		width: 800,
+		height: 400,
+		x: 950,
+		y: 0
+	});
+
+	win.loadURL(`file://${__dirname}/../renderer/subscribe.html`);
+	win.on('closed', onClosedSubscribe);
+
+	return win;
+}
+
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
+});
+
+app.on('quit', () => {
+});
+
+app.on('activate', () => {
+	if (!publishWindow) {
+		publishWindow = createPublishWindow();
+	}
+	if (!subscribeWindow) {
+		subscribeWindow = createSubscribeWindow();
+	}
+});
+
+app.on('ready', () => {
+	publishWindow = createPublishWindow();
+	//subscribeWindow = createSubscribeWindow();
+});
